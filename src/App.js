ca849020c0f6112bc3bdf257906b3c93dfeb8af5
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-03-11 17:48:58 +0330

    npm install graphql & use graphql in porject

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Users from './components/Users';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+     <Users/>
+     
+    </div>
+  );
+}
+
+export default App;
